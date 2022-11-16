b3e1c448b97faab4f2f707ef77486151a47e66d0
dovid-moshe-crow <dovid-moshe-crow@users.noreply.github.com>
2022-11-16 20:07:05 +0200

    Create world-of-tzedaka.ts

---
+++ pages/api/world-of-tzedaka.ts
@@ -0,0 +1,20 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = number;
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  res.status(200).json(parseEmail(req.query.email as string));
+}
+
+function parseEmail(text: string) {
+  return parseInt(
+    text
+      .split(" ")
+      .filter((x) => x.includes("$"))[0]
+      .replace("$", "")
+  );
+}
