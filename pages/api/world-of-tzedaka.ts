83806a5bdf249104d327d06e99981302c66c81fb
dovid-moshe-crow <dovid-moshe-crow@users.noreply.github.com>
2022-11-16 21:46:44 +0200

    fix amount and name

--- pages/api/world-of-tzedaka.ts
+++ pages/api/world-of-tzedaka.ts
@@ -1,7 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = number;
+type Data = {
+  amount: number;
+  name: string;
+};
 
 export default function handler(
   req: NextApiRequest,
@@ -11,10 +14,19 @@ export default function handler(
 }
 
 function parseEmail(text: string) {
-  return parseInt(
+  const amount = parseInt(
     text
       .split(/\s+/)
       .filter((x) => x.includes("$"))[0]
       .replace("$", "")
   );
+
+  const arr = text.split("\n");
+
+  const name = arr[arr.indexOf("Billing address") + 1];
+
+  return {
+    amount,
+    name,
+  };
 }
