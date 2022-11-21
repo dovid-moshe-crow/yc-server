f4fb3e7103e65d64cad9490770932ae6a186aefd
dovid-moshe-crow <dovid-moshe-crow@users.noreply.github.com>
2022-11-21 17:47:16 +0200

    pdfs

---
+++ pages/agra.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function agra() {
+  return <div>agra</div>;
+}
+
+export default agra;
---
+++ pages/api/agra.ts
@@ -0,0 +1,24 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import fs from "fs";
+type Data = Buffer;
+
+const json = JSON.parse(
+  fs.readFileSync(`.../../data/agra-index-data.json`).toString()
+);
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  //const file = fs.readFileSync(`.../../pdfs/${req.query.id}.pdf`,{encoding:"base64"});
+  res.setHeader("Content-Type", "application/pdf");
+  res.setHeader(
+    "Content-Disposition",
+    `inline; filename=${
+      encodeURIComponent(json[parseInt(req.query.id as string) - 1].paragraph.number)
+    }.pdf`
+  );
+  res.status(200).send(fs.readFileSync(`.../../pdfs/${req.query.id}.pdf`));
+}
