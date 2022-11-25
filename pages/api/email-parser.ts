6ede5dea30d0a6fd8d1480c4ec887106bec79c53
dovid-moshe-crow <dovid-moshe-crow@users.noreply.github.com>
2022-11-25 09:42:03 +0200

    מזהה הארגון

--- pages/api/email-parser.ts
+++ pages/api/email-parser.ts
@@ -18,7 +18,20 @@ function parseEmail(text: string) {
     .filter((x) => x.trim() != "" && x.includes(":"))
     .forEach((x) => {
       if (x.includes("עבור")) {
-        data["עבור"] = x.split("עבור")[1].trim().replace(":", "");
+        const forData = x.split("עבור")[1].trim().replace(":", "");
+        data["עבור"] = forData;
+        let forId = "";
+        if (forData.includes("חכמי")) {
+          forId = "6843c96d-6c75-4229-b62e-7a2a640e11ef";
+        } else if (forData.includes("אור אפרים")) {
+          forId = "3b57b079-684c-443c-9c2f-535b3a8d341e";
+        } else if (forData.includes("סומך")) {
+          forId = "ba86845e-50fd-4a19-890e-201e1668e1c9";
+        } else if (forData.includes("יפה ותמה")) {
+          forId = "9c82dc70-172c-4325-aa40-62682fe55803";
+        }
+
+        data["מזהה הארגון"] = forId;
       } else {
         data[x.split(":")[0]] = x.split(":")[1];
       }
